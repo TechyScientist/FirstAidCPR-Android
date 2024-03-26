66787d5f7c83fbde550002e0206371f3efa4515b
Johnny Console <johnny.console@hotmail.com>
2024-03-26 10:23:52 -0400

    Enable options menu on MainActivity and handle about button presses

--- app/src/main/java/com/johnnyconsole/android/firstaidcpr/MainActivity.kt
+++ app/src/main/java/com/johnnyconsole/android/firstaidcpr/MainActivity.kt
@@ -2,10 +2,31 @@ package com.johnnyconsole.android.firstaidcpr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about) {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.aboutTitle)
+                .setMessage(Html.fromHtml(getString(R.string.aboutMessage)))
+                .setNeutralButton(R.string.dismiss, null)
+                .create()
+                .show()
+        }
+        return true
     }
 }
